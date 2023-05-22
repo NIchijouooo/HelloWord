c86f589558d0b63cc39426d658b474877f5b6937
1luotonggui <1luotonggui@163.com>
2023-05-22 14:24:41 +0800

    #合并了IOT修改好配置文件下发

--- setting/FeisjyUpAndDownLoadFile.go
+++ setting/FeisjyUpAndDownLoadFile.go
@@ -14,6 +14,58 @@ import (
 	"path/filepath"
 )
 
+//func DownloadFileFeisjy(fileSavePath string, fileName string, url string) error {
+//	var req *http.Request = nil
+//	var err error
+//
+//	// 实例化一个http client对象
+//	client := &http.Client{}
+//	// 实例化一个http Get Request对象
+//	req, err = http.NewRequest("GET", url, nil)
+//
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("http.NewRequest Fail! %v", err))
+//	}
+//
+//	req.Header.Add("Cookie", "SHAREJSESSIONID=f0bae696-f1be-4df8-a5e1-10ba646b7ff0")
+//
+//	// 将http Get 请求对象发送服务器上 若发送成功则会得到响应
+//	res, err := client.Do(req)
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("client.Do Fail! %v", err))
+//	}
+//	defer res.Body.Close()
+//
+//	// 读取响应体
+//	body, err := ioutil.ReadAll(res.Body)
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("ioutil.ReadAll Fail! %v", err))
+//	}
+//
+//	// 判断是否路径是否存在，不存在则递归创建
+//	utils.DirIsExist(fileSavePath)
+//
+//	filePath := fmt.Sprintf("%s%s", fileSavePath, fileName)
+//
+//	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+//	defer file.Close()
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("文件打开失败[%s]! %v", filePath, err))
+//	} else {
+//		// 创建一个带缓冲区的写入器
+//		write := bufio.NewWriter(file)
+//		// 将响应体写入文件中
+//		_, err = write.WriteString(string(body))
+//		fmt.Printf(">>>>>>")
+//		if err != nil {
+//			return err
+//		}
+//		write.Flush()
+//	}
+//
+//	return nil
+//}
+
 func DownloadFileFeisjy(fileSavePath string, fileName string, url string, maxRetries int) error {
 
 	// 实例化一个http client对象
@@ -102,6 +154,65 @@ func DownloadFileFeisjy(fileSavePath string, fileName string, url string, maxRet
 	}
 
 	return errors.New(fmt.Sprintf("下载失败，已达到最大重试次数：%d", maxRetries))
+}
+
+/*
+*
+上传网关配置到云平台,先将配置文件压缩成一个压缩包再上传
+压缩的文件列表暂时写死测试
+url=上传接口
+deviceId = 云平台下发的设备id,再返回给云平台
+*/
+func UpLoadGatewayConfigFeisjy(url string, deviceId string) (string, error) {
+	fileName := "current.zip"
+	dir := "./tmp/"
+	filePath := dir + fileName
+	err := utils.CompressFilesToZip([]string{"./selfpara/collInterface.json",
+		"./selfpara/reportModel.json",
+		"./selfpara/reportServiceParamListFeisjyIot.json",
+		"./selfpara/commInterfaceProtocol.json"}, filePath)
+	if err != nil {
+		return "", err
+	}
+
+	//创建一个新的 multipart form
+	requestBody := &bytes.Buffer{}
+	writer := multipart.NewWriter(requestBody)
+	_ = writer.WriteField("deviceId", deviceId)
+	var file fs.File
+	var part io.Writer
+	file, err = os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("UpLoadFileFeisjy -> os.Open() Faile! : %s", err.Error()))
+	}
+	defer file.Close()
+
+	// 创建一个新的表单字段，并将文件内容写入其中
+	part, err = writer.CreateFormFile("fileName", filepath.Base(filePath))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("UpLoadFileFeisjy -> writer.CreateFormFile() Faile! : %s", err.Error()))
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("UpLoadFileFeisjy -> io.Copy() Faile! : %s", err.Error()))
+	}
+
+	// 关闭 multipart form 并设置 Content-Type
+	err = writer.Close()
+	contentType := writer.FormDataContentType()
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("UpLoadFileFeisjy -> writer.Close() Faile! : %s", err.Error()))
+	}
+
+	// 发送 HTTP POST 请求并上传文件
+	response, err := http.Post(url, contentType, requestBody)
+	if err != nil {
+		fmt.Println("Failed to upload file:", err)
+		return "", errors.New(fmt.Sprintf("UpLoadFileFeisjy -> http.Post() Faile! : %s", err.Error()))
+	}
+	defer response.Body.Close()
+	return fileName, nil
 }
 
 func UpLoadFileFeisjy(filePath string, url string, uniqueCode string, deviceAddr string) error {
