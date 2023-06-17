586f2aeda9e4cd1c71f94cb2ecf84c2d7bca0af8
liping321 <1648948548@qq.com>
2023-06-17 14:37:26 +0800

    feat：上报通道，添加服务增加录入项，指定上报通道和上报网卡

---
+++ webroot/assets/product.33d392be.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index.bef3255d.js";var e={getGatewaySN:e=>t.request({url:"/product/sn",method:"get",data:e}),setGatewaySN:e=>t.request({url:"/product/sn",method:"post",data:e})};export{e as P};
