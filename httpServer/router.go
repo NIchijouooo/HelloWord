217ed65063d28141f1bb7aecdaf10920ddf47d8d
Rui <veryrui@gmail.com>
2023-06-19 11:31:18 +0800

    fix: 修复路由重复导入控制器

--- httpServer/router.go
+++ httpServer/router.go
@@ -61,9 +61,6 @@ func RouterWeb(port string) {
 		emController := controllers.NewEMController()
 		emController.RegisterRoutes(&router.RouterGroup)
 
-		deviceController := controllers.NewDeviceController()
-		deviceController.RegisterRoutes(&router.RouterGroup)
-
 		/**
 		20230605
 		*/
