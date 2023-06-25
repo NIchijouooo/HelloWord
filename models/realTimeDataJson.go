ab2f16071fe0b546bf56e9870d435948017a8163
wusong <592719787@qq.com>
2023-06-25 18:20:26 +0800

    perf：组态配置后端

--- models/realTimeDataJson.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type RealTimeDataJson struct {
-	Id       int    `json:"id"`
-	Type     int    `json:"type"`
-	Code     int    `json:"code"`
-	Value    string `json:"value"`
-	DeviceId int    `json:"deviceId"`
-}
