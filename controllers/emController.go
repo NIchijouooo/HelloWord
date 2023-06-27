fb35c817a532b2d51fce3ce83a879370c56df10a
Rui <veryrui@gmail.com>
2023-06-27 16:56:46 +0800

    feat: 增加设备类型的字段

--- controllers/emController.go
+++ controllers/emController.go
@@ -286,6 +286,7 @@ func (c *EmController) AddEmDevice(ctx *gin.Context) {
 	emDevice.Name = addEmDevice.Name
 	emDevice.Label = addEmDevice.Label
 	emDevice.Addr = addEmDevice.Addr
+	emDevice.DeviceType = addEmDevice.DeviceType
 	// 判断是否有重名的设备，重名直接返回
 	emDevice.Name = addEmDevice.Name
 	emDeviceByName, _ := c.repo.GetEmDeviceByName(emDevice.Name)
