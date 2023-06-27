744d4ba289c35bb6351be0a2245a5bd2055c4d7f
wusong <592719787@qq.com>
2023-06-27 18:33:46 +0800

    perf：删除不用的方法

--- controllers/webHmiPageController.go
+++ controllers/webHmiPageController.go
@@ -20,56 +20,7 @@ func NewWebHmiPageController() *WebHmiPageController {
 }
 
 func (c *WebHmiPageController) RegisterRoutes(router *gin.RouterGroup) {
-	router.POST("/api/v2/webHmiPage/getWebHmiPageDeviceInfo", c.GetWebHmiPageDeviceInfo)
-	router.POST("/api/v2/webHmiPage/saveWebHmiPageDeviceInfo", c.SaveWebHmiPageDeviceInfo)
 	router.POST("/api/v2/webHmiPage/getIotWebHmiPageInfo", c.GetIotWebHmiPageInfo)
-}
-
-func (c *WebHmiPageController) GetWebHmiPageDeviceInfo(ctx *gin.Context) {
-	webHmiPageDeviceModel := models.WebHmiPageDeviceModel{}
-	if err := ctx.ShouldBindBodyWith(&webHmiPageDeviceModel, binding.JSON); err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	deviceId := webHmiPageDeviceModel.DeviceId
-
-	webHmiPageDeviceModelList, err := c.repo.GetWebHmiPageDeviceInfo(deviceId)
-	if err != nil {
-		ctx.JSON(http.StatusOK, model.ResponseData{
-			Code:    "1",
-			Message: "error" + err.Error(),
-			Data:    "",
-		})
-		return
-	}
-	ctx.JSON(http.StatusOK, model.ResponseData{
-		Code: "0",
-		Data: webHmiPageDeviceModelList,
-	})
-}
-
-func (c *WebHmiPageController) SaveWebHmiPageDeviceInfo(ctx *gin.Context) {
-
-	webHmiPageDeviceModelList := make([]*models.WebHmiPageDeviceModel, 0)
-
-	if err := ctx.ShouldBindBodyWith(&webHmiPageDeviceModelList, binding.JSON); err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	saveFlag, err := c.repo.SaveWebHmiPageDeviceInfo(webHmiPageDeviceModelList)
-	if err != nil {
-		ctx.JSON(http.StatusOK, model.ResponseData{
-			Code:    "1",
-			Message: "error" + err.Error(),
-			Data:    "",
-		})
-		return
-	}
-	ctx.JSON(http.StatusOK, model.ResponseData{
-		Code: "0",
-		Data: saveFlag,
-	})
 }
 
 func (c *WebHmiPageController) GetIotWebHmiPageInfo(ctx *gin.Context) {
